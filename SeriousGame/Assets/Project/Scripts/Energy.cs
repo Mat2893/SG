77340c7f05c7515e3e9cab727a586b5af327afdd
Mat2893 <mathieu.gerard.pro@gmail.com>
2016-05-27 15:16:28 -0400

    ADD beginning of the interface

---
+++ SeriousGame/Assets/Project/Scripts/Energy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Energy : MonoBehaviour
+{
+	private PlayerControl playerControl;	// Reference to the player control script.
+	private int previousScore = 0;			// The score in the previous frame.
+
+    private GameObject energyScript;
+    private int energy;
+
+    void Start()
+    {
+        energyScript = GameObject.Find("SpawnerAlly");
+        if(energyScript)
+        {
+            energy = (int) energyScript.GetComponent<SpawnerAlly>().getEnergy();
+        }
+        else
+        {
+            Debug.Log("Erreur script SpawnerAlly non trouvé");
+        }
+    }
+
+	void Update ()
+	{
+        energy = (int) energyScript.GetComponent<SpawnerAlly>().getEnergy();
+        // Set the score text.
+        GetComponent<GUIText>().text = "Energy: " + energy;
+	}
+
+}
