3b7cb7353a354e899b038d5cdc65c6284f16cf7f
Mat2893 <mathieu.gerard.pro@gmail.com>
2016-06-11 14:22:47 -0400

    ADD specialised unit after time

---
+++ SeriousGame/Assets/Project/Scripts/Vaccin.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Vaccin : MonoBehaviour {
+
+    public GameObject specialUnit;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(VaccinWait(10));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator VaccinWait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        specialUnit.SetActive(true);
+    }
+}
